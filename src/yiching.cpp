c4e7b99227d9c499ac8f81332137e36e88e799e0
Zeta <zeta@zeta2374.com>
2021-06-16 22:16:24 +0900

    v1.0

--- src/yiching.cpp
+++ src/yiching.cpp
@@ -1,6 +1,6 @@
 #include<gtkmm.h>
 #include<fstream>
-#include<iostream>
+//#include<iostream>
 #include<string>
 using namespace std;
 
@@ -18,7 +18,8 @@ struct Gwe
 const string chinese = "地雷水泽山火风天";
 
 string remove_head(string s) {
-	string to_find = s.find("，") != string::npos ? "，" : "：";//wierd utf-8 string
+	return s;
+	string to_find = (s.find("，") != string::npos) ? "，" : "：";//wierd utf-8 string
 	return s.substr(s.find(to_find) + 3);//not 1 byte, but 3 byte
 }
 
@@ -32,7 +33,7 @@ void parse_file() {
 		s = s.substr(pos + 9);
 		if(chinese.find(s.substr(0, 3), 0) == string::npos) 
 			s = s.substr(6, 3) + s.substr(6, 3) + s.substr(0, 3);
-		cout << s << endl;
+		//cout << s << endl;
 		char up = chinese.find(s.substr(0, 3), 0) / 3;
 		char down = chinese.find(s.substr(3, 3), 0) / 3;
 		g[i].name_ = s.substr(6);
@@ -51,8 +52,8 @@ void parse_file() {
 				g[i].hsang_[j] = remove_head(s);
 			}
 		}
-		cout << +g[i].gwe_ << g[i].name_ << g[i].gphrase_ << g[i].gsang_ << g[i].gdan_;
-		for(int j=0; j<6; j++) cout << g[i].hphrase_[j] << g[i].hsang_[j];
+		//cout << +g[i].gwe_ << g[i].name_ << g[i].gphrase_ << g[i].gsang_ << g[i].gdan_;
+		//for(int j=0; j<6; j++) cout << g[i].hphrase_[j] << g[i].hsang_[j];
 	}
 }
 
@@ -71,9 +72,14 @@ public:
 		for(int i=0; i<6; i++) grid_.attach(hyo_[1][5-i], 3, i+1);
 		for(int i=0; i<6; i++) grid_.attach(hyo_phrase_[1][5-i], 4, i+1);
 
-		for(int i=0; i<6; i++) hyo_[0][i].set_image(yin_img_);
+		for(int i=0; i<6; i++) for(int j=0; j<2; j++) {
+			pix_[i][j][0].set(yin_img_.get_pixbuf()->scale_simple(300, 70, Gdk::INTERP_BILINEAR));
+			pix_[i][j][1].set(yang_img_.get_pixbuf()->scale_simple(300, 70, Gdk::INTERP_BILINEAR));
+		}
+		for(int i=0; i<6; i++) hyo_[0][i].set_image(pix_[i][0][0]);
 		change_.set_tooltip_text("變爻");
 		show_all_children();
+		connect_event();
 	}
 protected:
 	Gtk::Grid grid_;
@@ -81,10 +87,81 @@ protected:
 	Gtk::CheckButton check_[6];
 	Gtk::Label  hyo_phrase_[2][6], phrase_[2], change_{"변효"};
 	Gtk::Button hyo_[2][6];
-	const Gtk::Image yin_img_{"yin.png"}, yang_img_{"yang.png"};
+	bool yinyang_[6] = {false, };
+	Gtk::Image yin_img_{"yin.png"}, yang_img_{"yang.png"}, pix_[6][2][2];
 private:
-	string yin_ = "<span size=\"xx-large\">\u268b</span>",
-				 yang_ = "<span size=\"xx-large\">\u268a</span>";
+//	string yin_ = "<span size=\"xx-large\">\u268b</span>",
+//				 yang_ = "<span size=\"xx-large\">\u268a</span>";
+	void connect_event() {
+		for(int i=0; i<6; i++) hyo_[0][i].signal_clicked().connect([this, i]() {
+				yinyang_[i] = !yinyang_[i];
+				hyo_[0][i].set_image(yinyang_[i] ? pix_[i][0][1] : pix_[i][0][0]);
+		});
+		bt_.signal_clicked().connect([this]() {
+				char c = 0, d = 0;
+				int changed_count = 0;
+				for(int i=0; i<6; i++) c += (yinyang_[i] ? 1 : 0) << i;
+				for(int i=0; i<6; i++) if(check_[i].get_active()) {
+					changed_count++;
+					d +=  1 << i;
+				} 
+				char e = c ^ d;
+				for(int i=0; i<6; i++) hyo_[1][i].set_image(e & (1 << i) ? pix_[i][1][1] : pix_[i][1][0]);
+				for(int i=0; i<64; i++) {
+					if(g[i].gwe_ == c) {
+						phrase_[0].set_markup(g[i].gphrase_);
+						phrase_[0].set_tooltip_text(g[i].gdan_ + '\n' + g[i].gsang_);
+						for(int j=0; j<6; j++) {
+							hyo_phrase_[0][j].set_markup(g[i].hphrase_[j]);
+							hyo_phrase_[0][j].set_tooltip_text(g[i].hsang_[j]);
+						}
+					}
+					if(g[i].gwe_ == e) {
+						phrase_[1].set_markup(g[i].gphrase_);
+						phrase_[1].set_tooltip_text(g[i].gdan_ + '\n' + g[i].gsang_);
+						for(int j=0; j<6; j++) {
+							hyo_phrase_[1][j].set_markup(g[i].hphrase_[j]);
+							hyo_phrase_[1][j].set_tooltip_text(g[i].hsang_[j]);
+						}
+					}
+				}
+				switch(changed_count) {
+					case 0: phrase_[0].set_markup(bluefy(phrase_[0].get_label()));
+									break;
+					case 1: hyo_phrase_[0][log2(d)].set_markup(bluefy(hyo_phrase_[0][log2(d)].get_label()));
+									break;
+					case 2: hyo_phrase_[0][get_upper(d)].set_markup(bluefy(hyo_phrase_[0][get_upper(d)].get_label()));
+									break;
+					case 3: phrase_[0].set_markup(bluefy(phrase_[0].get_label()));
+									phrase_[1].set_markup(bluefy(phrase_[1].get_label()));
+									break;
+					case 4: hyo_phrase_[1][get_lower(flip(d))].set_markup(
+											bluefy(hyo_phrase_[1][get_lower(flip(d))].get_label()));
+									break;
+					case 5: hyo_phrase_[1][log2(flip(d))].set_markup(
+											bluefy(hyo_phrase_[1][log2(flip(d))].get_label()));
+									break;
+					case 6: phrase_[1].set_tooltip_text(bluefy(phrase_[1].get_tooltip_text()));
+									break;
+				}
+		});
+	}
+	string bluefy(string s) {
+		return "<span foreground=\"blue\">" + s + "</span>";
+	}
+	int log2(char c) {
+		for(int i=0; i<6; i++) if(1 << i == c) return i;
+		throw "do not reach here";
+	}
+	int get_upper(char c) {
+		for(int i=0; i<6; i++) if(0b100000 >> i & c) return 5-i;
+	}
+	int get_lower(char c) {
+		for(int i=0; i<6; i++) if(1 << i & c) return i;
+	}
+	char flip(char c) {
+		return ~c & 0b00111111;
+	}
 };
 
 int main(int ac, char** av)
